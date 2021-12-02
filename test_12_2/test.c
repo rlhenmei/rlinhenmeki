42fff3488d78f6c457fe41dde04f0bda6a4c303f
rlhenmei <1944419099@qq.com>
2021-12-02 22:49:53 +0800

    c语言学习

---
+++ test_12_2/test.c
@@ -0,0 +1,95 @@
+ #define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//    int a, b, c, d;
+//    scanf("%d %d %d %d", &a, &b, &c, &d);
+//    printf("%.1f", a * 0.2 + b * 0.1 + c * 0.2 + d * 0.5);
+//    return 0;
+//}
+//int main()
+//{
+//    int n = 0;
+//    int i = 0;
+//    scanf("%d", &n);
+//    for (i = 0; i < n; i++)
+//    {
+//        puts("Happy new year!Good luck!");
+//    }
+//    return 0;
+//}
+//int main()
+//{
+//    int a, b, c;
+//    int max = 0;
+//    scanf("%d %d %d", &a, &b, &c);
+//    max = a > b ? a : b;
+//    max = max > c ? max : c;
+//    printf("%d", max);
+//    return 0;
+//}
+//int main()
+//{
+//    int a, b, c;
+//    scanf("%d %d %d", &a, &b, &c);
+//    printf("%d/%d/%d", c, b, a);
+//    return 0;
+//}
+//int main()
+//{
+//    char a = 73;
+//    char b = 32;
+//    char c = 99;
+//    char d = 97;
+//    char e = 110;
+//    char f = 32;
+//    char g = 100;
+//    char h = 111;
+//    char i = 32;
+//    char j = 105;
+//    char k = 116;
+//    char l = 33;
+//    printf("%c%c%c%c%c%c%c%c%c%c%c%c", a, b, c, d, e, f, g, h, i, j, k, l);
+//    return 0;
+//}
+//int main()
+//{
+//int arr[5] = { 0 };
+//int arr1[5] = { 0 };
+//int i = 0;
+//for (i = 0; i < 5; i++)
+//{
+//    scanf("%d", &arr[i]);
+//}
+//int j = 0;
+//for (i = 0; i < 5; i++)
+//{
+//    int count = 0;
+//    for (j = 0; j < 5; j++)
+//    {
+//        if (arr[i] < arr[j])
+//        {
+//            count++;
+//        }
+//    }
+//    arr1[i] = 1 + count;
+//}
+//printf("[");
+//for (i = 0; i < 5; i++)
+//{
+//    while (i != 5)
+//    {
+//        switch (arr[i])
+//        {
+//        case 1:printf("Gold Medal "); break;
+//        case 2:printf("Silver Medal "); break;
+//        case 3:printf("Bronze Medal "); break;
+//        case 4:printf("4 "); break;
+//        case 5:printf("5 "); break;
+//        }
+//        break;
+//    }
+//    
+//}
+//printf("]");
+//}
