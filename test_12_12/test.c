e32ca100b5aa0c71c5c82f717a21849f5b859ac6
rlhenmei <1944419099@qq.com>
2021-12-14 23:28:55 +0800

    c语言学习

---
+++ test_12_12/test.c
@@ -0,0 +1,88 @@
+ #define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int arr1[40000] = { 0 };
+//int main()
+//{
+//	char arr[200] = { 0 };
+//	gets(arr);
+//	int n = strlen(arr);
+//	int count = 0;
+//	int i = 0;
+//	int j = 0;
+//	while (--n)
+//	{
+//		for (i = 0; i < n; i++)
+//		{
+//			if (arr[i] == '0')
+//			{
+//				count++;
+//			}
+//			else if (arr[i] == '1')
+//			{
+//				arr1[j] = count;
+//				j++;
+//				count = 1;
+//			}
+//		}
+//		for (i = 0; i < n; i++)
+//		{
+//			arr[i] = 0;
+//		}
+//		gets(arr);
+//	}
+//	printf("%d", n);
+//	for (i = 0; i < 40000; i++)
+//	{
+//		if (arr[i] != 0)
+//			printf("%d", arr[i]);
+//	}
+//	return 0;
+//}
+//#include<string.h>
+//char arr[40000] = { 0 };
+//int main()
+//{
+//	char arr1[200] = { 0 };
+//	int i = 0;
+//	int count = 0;
+//	char a = '0';
+//	gets(arr1);
+//	int n = strlen(arr1);
+//	strcat(arr, arr1);
+//	for (i = 2; i <= n; i++)
+//	{
+//		gets(arr1);
+//		strcat(arr, arr1);
+//	}
+//	printf("%d", n);
+//	for (i = 0; i <= strlen(arr); i++)
+//	{
+//		if (arr[i] == a)
+//			count++;
+//		else
+//		{
+//			a = arr[i];
+//			printf(" %d", count);
+//			count = 1;
+//		}
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	char a[200] = { 0 };
+//	gets(a);
+//	printf("%c", a[0]);
+//	gets(a);
+//	printf("%c", a[4]);
+//	return 0;
+//}
+int main()
+{
+	char arr[256] = { 0 };
+	gets(arr);
+
+
+
+	return 0;
+}
