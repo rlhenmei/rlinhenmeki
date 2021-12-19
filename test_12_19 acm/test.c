6e5062aff826f73f9bb7d22cad7d7518fa43c374
rlhenmei <1944419099@qq.com>
2021-12-19 22:25:02 +0800

    c语言学习

---
+++ test_12_19 acm/test.c
@@ -0,0 +1,46 @@
+ #define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	while (n--)
+//	{
+//		printf("Hello ACM!\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int n = 0;
+//	
+//	scanf("%d", &n);
+//	while (n--)
+//	{
+//		int a, b, c, d, max=0, i = 0;
+//		scanf("%d %d %d %d", &a, &b, &c, &d);
+//		max = a > b ? a : b;
+//		max = max > c ? max : c;
+//		while (d)
+//		{
+//			int m = d % 10;
+//			i = i * 10 + m;
+//			d /= 10;
+//		}
+//		printf("%d\n", i + max);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int n, a, b,i;
+//	scanf("%d %d %d", &n, &a, &b);
+//
+//	return 0;
+//}
+int main()
+{
+	char arr[1001] = { 0 };
+	gets(arr);
+
+}
