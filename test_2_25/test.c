c6f5e20d814c769e7ce6736daf792a7431b118ba
rlhenmei <1944419099@qq.com>
2022-02-25 22:53:56 +0800

    c语言学习

---
+++ test_2_25/test.c
@@ -0,0 +1,41 @@
+ 
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<math.h>
+//int Perfect_number(int num) {
+//    int sum = 0;
+//    for (int j = 2; j <= sqrt(num); j++)
+//    {
+//        if (num % j == 0)
+//        {
+//            sum += j;
+//            if (j != sqrt(num))
+//                sum += num / j;
+//        }
+//    }
+//    if (sum == num)
+//        return 1;
+//    return 0;
+//}
+//int main()
+//{
+//    int n;
+//    while (~scanf("%d", &n)) {
+//        int count = 0;
+//        for (int i = 2; i <= n; i++) {
+//            if (Perfect_number(i))
+//                count++;
+//        }
+//        printf("%d\n", count);
+//    }
+//    return 0;
+//}
+int main()
+{
+
+	printf("%d", 0xf);
+
+
+
+	return 0;
+}
