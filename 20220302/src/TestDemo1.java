b8cd7c392b0b23ce142a4c79e91d12edad0447f8
rlhenmei <1944419099@qq.com>
2022-03-03 19:45:53 +0800

    java学习

---
+++ 20220302/src/TestDemo1.java
@@ -0,0 +1,17 @@
+/**
+ * Created with Intellij IDEA
+ * Description:
+ * User:半截的诗
+ * DATE:2022-03-02
+ * Time:21:14
+ */
+public class TestDemo1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("abcd");
+        }
+        int a=10;
+        System.out.println(a);
+    }
+}
